8e5832cb2551267806bfbf830f570159c28fe767
VIKTOOOOOOOOOOOR <viktorgyorgy@gmail.com>
2024-01-24 14:18:40 +0100

    secret file and images for the Readme

--- backend/configuration/secret.js
+++ backend/configuration/secret.js
@@ -1,3 +1,3 @@
-export default secret = "<random_hash_key_for_password>";
+export default password_secret_hash = "<random_hash_key_for_password>";
 export const mongodb_uri = "<mongo_db_uri>";
 export const edenai_bearer = "<edenai-bearer-secret>"
