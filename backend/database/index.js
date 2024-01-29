d0123b3e54feebab1138ebece56238f723e5a2cd
VIKTOOOOOOOOOOOR <viktorgyorgy@gmail.com>
2024-01-30 00:49:42 +0100

    added database name into config file

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,11 +1,11 @@
 import { MongoClient} from 'mongodb';
-import { mongodb_uri } from '../configuration/secret';
+import { mongodb_uri, mongodb_database_name } from '../configuration/secret';
 const dbCollections = {};
 
 const client = new MongoClient(mongodb_uri);
 await client.connect();
-const collection = client.db("allamvizsga").collection("users");
+const collection = client.db(mongodb_database_name).collection("users");
 dbCollections.users = collection;
-dbCollections.passwordCodes = client.db("allamvizsga").collection("passwordCodes");
-dbCollections.profiles = client.db("allamvizsga").collection("profiles");
+dbCollections.passwordCodes = client.db(mongodb_database_name).collection("passwordCodes");
+dbCollections.profiles = client.db(mongodb_database_name).collection("profiles");
 export default dbCollections;
